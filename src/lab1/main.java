bcf80e55f01e55917d945588b9875e90df37f786
aebdulla <144229060+aebdulla@users.noreply.github.com>
2023-09-06 14:57:20 +0400

    feature B added

--- src/lab1/main.java
+++ src/lab1/main.java
@@ -4,4 +4,8 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
+
+	public void featureB(){
+		System.out.println("Thank you Yousef :)");
+	}
 }
